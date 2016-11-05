e2cb7dd8d29acdf8b2c5eaaf3a704669531fd82c
Josh <joshkulchin@gmail.com>
2016-11-05 18:34:35 +0000

    Finally passed!

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,38 @@
 class Bookstore(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.books = []
 
-
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def get_books(self):
+        return self.books
+    
+    def search_books(self, title=None, author=None):
+        results = []
+        if title==None:
+            for book in self.books:
+                if book.author == author:
+                    results.append(book)
+            return results
+        else:
+            for book in self.books:
+                if title.lower() in book.title.lower():
+                    results.append(book)
+            return results
+        
 class Author(object):
-    pass
-
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def get_books(self):
+        return self.books
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.books.append(self)
